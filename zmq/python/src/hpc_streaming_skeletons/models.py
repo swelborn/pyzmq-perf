da1af9ee9cd51dd31f5be40a1f7fc3a395a4f58f
swelborn <55868530+swelborn@users.noreply.github.com>
2025-07-02 07:57:41 -0400

    remove client/server -> worker/coordinator

--- zmq/python/src/hpc_streaming_skeletons/models.py
+++ zmq/python/src/hpc_streaming_skeletons/models.py
@@ -1,4 +1,24 @@
+from enum import Enum
+
 from pydantic import BaseModel
+
+
+class Role(str, Enum):
+    sender = "sender"
+    receiver = "receiver"
+
+
+class WorkerCreate(BaseModel):
+    worker_id: str
+    role: Role
+
+
+class CoordinationSignal(str, Enum):
+    CONFIG = "CONFIG"
+    START = "START"
+    FINISH = "FINISH"
+
+
 class TestConfig(BaseModel):
     count: int  # number of messages to send
     size: int  # size of messages
@@ -7,3 +27,54 @@ class TestConfig(BaseModel):
     rcvhwm: int  # puller hwm
     sndhwm: int  # sender hwm
     test_number: int
+
+
+class TestResult(BaseModel):
+    worker_id: str
+    role: Role
+    config: TestConfig
+    messages_sent: int | None = None
+    messages_received: int | None = None
+    throughput_mbps: float
+
+
+class WorkerState(str, Enum):
+    CONNECTING_TO_COORDINATOR = "CONNECTING_TO_COORDINATOR"
+    CONNECTED_TO_SYNC = "CONNECTED_TO_SYNC"
+    RECEIVED_CONFIG = "RECEIVED_CONFIG"
+    READY_TO_TEST = "READY_TO_TEST"
+    RUNNING_TEST = "RUNNING_TEST"
+    FINISHED_TEST = "FINISHED_TEST"
+
+    def transition_allowed(self, next_state: "WorkerState") -> bool:
+        allowed_transitions = {
+            WorkerState.CONNECTING_TO_COORDINATOR: [WorkerState.CONNECTED_TO_SYNC],
+            WorkerState.CONNECTED_TO_SYNC: [WorkerState.RECEIVED_CONFIG],
+            WorkerState.RECEIVED_CONFIG: [WorkerState.READY_TO_TEST],
+            WorkerState.READY_TO_TEST: [WorkerState.RUNNING_TEST],
+            WorkerState.RUNNING_TEST: [WorkerState.FINISHED_TEST],
+            WorkerState.FINISHED_TEST: [WorkerState.RECEIVED_CONFIG],
+        }
+        return next_state in allowed_transitions.get(self, [])
+
+
+class Worker(WorkerCreate):
+    id: bytes
+    state: WorkerState = WorkerState.CONNECTING_TO_COORDINATOR
+    pair_id: bytes | None = None
+    test_number: int | None = None
+
+
+class WorkerUpdate(BaseModel):
+    state: WorkerState
+    test_number: int | None = None
+    result: TestResult | None = None
+
+
+class Mode(str, Enum):
+    coordinator = "coordinator"
+    worker = "worker"
+
+
+class SetupInfo(BaseModel):
+    data_port: int
---
+++ zmq/python/src/hpc_streaming_skeletons/worker.py
@@ -0,0 +1,194 @@
+import json
+import logging
+import time
+
+import zmq
+from hpc_streaming_skeletons.models import (
+    CoordinationSignal,
+    Role,
+    SetupInfo,
+    TestResult,
+    WorkerCreate,
+    WorkerState,
+    WorkerUpdate,
+)
+
+from .models import TestConfig
+
+
+def get_worker_logger(worker_id: str, level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(f"worker.{worker_id}")
+    if not logger.hasHandlers():
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            f"%(asctime)s [%(levelname)s] [{worker_id}] %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    logger.propagate = False
+    logger.setLevel(level)
+    return logger
+
+
+def send_update(socket: zmq.Socket, worker_id: str, update: WorkerUpdate):
+    socket.send(update.model_dump_json().encode())
+    if not socket.recv_string() == "ACK":
+        raise RuntimeError(
+            f"[{worker_id}] did not receive ACK from coordinator after update."
+        )
+
+
+def worker(
+    role: Role,
+    worker_id: str,
+    coordinator_ip: str,
+    sender_bind: bool,
+    log_level: int = logging.INFO,
+):
+    logger = get_worker_logger(worker_id, log_level)
+    ctx = zmq.Context()
+
+    _worker_model = WorkerCreate(role=role, worker_id=worker_id)
+
+    # Register with coordinator
+    req_socket = ctx.socket(zmq.REQ)
+    req_socket.connect(f"tcp://{coordinator_ip}:5555")
+    req_socket.send(_worker_model.model_dump_json().encode())
+
+    # Get peer info and sync address
+    setup_info_raw = req_socket.recv()
+
+    setup_info = SetupInfo.model_validate_json(setup_info_raw)
+    data_port = setup_info.data_port
+    sync_address = f"tcp://{coordinator_ip}:5556"
+    logger.debug(f"Received setup info: \n{setup_info.model_dump_json(indent=2)}")
+
+    # Subscribe to sync signals
+    sub_socket = ctx.socket(zmq.SUB)
+    sub_socket.connect(sync_address)
+    sub_socket.setsockopt(zmq.SUBSCRIBE, b"")
+    time.sleep(1)
+
+    logger.debug("connected to coordinator and sync channel.")
+
+    update = WorkerUpdate(state=WorkerState.CONNECTED_TO_SYNC)
+    send_update(req_socket, worker_id, update)
+    logger.debug(f"updated state to {update.state}.")
+
+    while True:
+        # Wait for test config
+        topic, config_json = sub_socket.recv_multipart()
+        if topic.decode() == CoordinationSignal.FINISH.value:
+            break
+
+        config = TestConfig(**json.loads(config_json.decode()))
+        logger.debug("Received config.")
+        update = WorkerUpdate(
+            state=WorkerState.RECEIVED_CONFIG, test_number=config.test_number
+        )
+        send_update(req_socket, worker_id, update)
+
+        # Setup data socket
+        if role == Role.sender:
+            data_socket = ctx.socket(zmq.PUB if config.pub else zmq.PUSH)
+            data_socket.setsockopt(zmq.SNDHWM, config.sndhwm)
+            if sender_bind:
+                data_socket.bind(f"tcp://*:{data_port}")
+            else:
+                data_socket.connect(f"tcp://{coordinator_ip}:{data_port}")
+        else:
+            data_socket = ctx.socket(zmq.SUB if config.pub else zmq.PULL)
+            if config.pub:
+                data_socket.setsockopt_string(zmq.SUBSCRIBE, "")
+            data_socket.setsockopt(zmq.RCVHWM, config.rcvhwm)
+            if not sender_bind:
+                data_socket.bind(f"tcp://*:{data_port}")
+            else:
+                data_socket.connect(f"tcp://{coordinator_ip}:{data_port}")
+        time.sleep(1)  # Allow time for connections to establish
+
+        # Signal ready
+        update = WorkerUpdate(
+            state=WorkerState.READY_TO_TEST, test_number=config.test_number
+        )
+        send_update(req_socket, worker_id, update)
+
+        # Wait for start signal
+        if not sub_socket.recv_string() == CoordinationSignal.START.value:
+            raise RuntimeError(f"Worker {worker_id} did not receive START signal.")
+        logger.info(f"Starting test {config.test_number}.")
+
+        # Run test
+        result_data = run_test(role, config, data_socket)
+        result = TestResult(
+            worker_id=worker_id,
+            role=role,
+            config=config,
+            **result_data,
+        )
+
+        # Send results
+        update = WorkerUpdate(
+            state=WorkerState.FINISHED_TEST,
+            test_number=config.test_number,
+            result=result,
+        )
+        send_update(req_socket, worker_id, update)
+        data_socket.close()
+        logger.info(f"Finished test {config.test_number} and sent results.")
+
+    logger.info("Shutting down.")
+    ctx.destroy()
+
+
+def calculate_throughput(
+    messages: int, size: int, start_time: float, end_time: float
+) -> float:
+    elapsed_time = end_time - start_time
+    if elapsed_time <= 0:
+        return 0.0
+    return (messages * size * 8) / (elapsed_time * 1024 * 1024)  # Mbps
+
+
+def run_test(role: Role, config: TestConfig, data_socket: zmq.Socket):
+    data = b" " * config.size
+    copy = not config.zero_copy
+
+    def send():
+        start_time = 0.0
+        first_message_sent = False
+        for _ in range(config.count):
+            data_socket.send(data, copy=copy)
+            if not first_message_sent:
+                first_message_sent = True
+                start_time = time.time()
+        end_time = time.time()
+        messages_sent = config.count - 1
+        throughput = calculate_throughput(
+            messages_sent, config.size, start_time, end_time
+        )
+        return {"messages_sent": messages_sent, "throughput_mbps": throughput}
+
+    def receive():
+        start_time = 0.0
+        # start at -1 to account for the first message not being counted
+        messages_received = -1
+        first_message_received = False
+        for _ in range(config.count):
+            data_socket.recv(copy=copy)
+            if not first_message_received:
+                first_message_received = True
+                start_time = time.time()
+            messages_received += 1
+        end_time = time.time()
+        throughput = calculate_throughput(
+            messages_received, config.size, start_time, end_time
+        )
+        return {"messages_received": messages_received, "throughput_mbps": throughput}
+
+    method_map = {
+        Role.sender: send,
+        Role.receiver: receive,
+    }
+
+    return method_map[role]()
